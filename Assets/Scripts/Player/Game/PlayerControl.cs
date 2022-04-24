48c2c854dfe9a190a12013b7c589d34688c10b61
zombiehunternr1 <44666850+zombiehunternr1@users.noreply.github.com>
2022-04-24 19:43:31 +0200

    Player movement, rotation and animation implemented
    - Player can now move around and rotate towards the direction the player is moving towards
    - Adjusting the players animation from idle to running depending weither or not the player is moving or not
    - Added in comments above the current function to explain what these functions do

--- Assets/Scripts/Player/Game/PlayerControl.cs
+++ Assets/Scripts/Player/Game/PlayerControl.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,7 +9,7 @@ public class PlayerControl : MonoBehaviour
     [SerializeField]
     private float movementSpeed;
     [SerializeField]
-    private float animationSpeed;
+    private float rotationSpeed;
     private Rigidbody rB;
     private Vector2 inputDirection;
     private Vector3 moveDirection;
@@ -20,11 +21,19 @@ public class PlayerControl : MonoBehaviour
         anim = GetComponent<Animator>();
     }
 
+    //Updates the players position and rotation depending on the movement direction
     private void FixedUpdate()
     {
-        rB.MovePosition(rB.position + (moveDirection * movementSpeed) * Time.fixedDeltaTime);
+        rB.MovePosition(transform.position + (moveDirection * movementSpeed) * Time.fixedDeltaTime);
+        anim.SetFloat("Speed", rB.velocity.magnitude);
+        if(moveDirection != Vector3.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
+            rB.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
+        }
     }
 
+    //Gets the players movement input and stores it into a Vector3
     public void OnMovement(InputAction.CallbackContext context)
     {
         inputDirection = context.ReadValue<Vector2>();
